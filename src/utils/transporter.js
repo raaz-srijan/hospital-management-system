9042a9921bce096462c899dd7ed565e2a4bc94f5
raaz-srijan <srijannospace@gmail.com>
2025-12-24 18:49:20 +0545

    changed Port

--- src/utils/transporter.js
+++ src/utils/transporter.js
@@ -3,6 +3,8 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  port:466,
+  secure:true,
   auth: {
     user: process.env.SMTP_EMAIL,
     pass: process.env.SMTP_PASS,
