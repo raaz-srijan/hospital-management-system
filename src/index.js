79b2b49e838956543d1b8ed78289b62d43ef3970
raaz-srijan <srijannospace@gmail.com>
2025-12-25 18:30:03 +0545

    vercel

--- src/index.js
+++ src/index.js
@@ -1,50 +1,33 @@
-  const express = require('express');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-  const app = express();
+const app = express();
 
-  app.use(express.json());
+app.use(express.json());
+app.use(cors());
 
-  require("dotenv").config();
+const connectDb = require("./config/connectDb");
 
-  const cors = require("cors");
+connectDb();
 
-  app.use(cors());
+app.get("/", (req, res) => {
+  res.send("server is live");
+});
 
-  const PORT = process.env.PORT || 8000;
-  const connectDb = require("./config/connectDb");
+app.use("/api/user", require("./routes/userRoute"));
+app.use("/api/roles", require("./routes/roleRoute"));
+app.use("/api/permissions", require("./routes/permissionRoute"));
+app.use("/api/patients", require("./routes/patientRoute"));
+app.use("/api/doctors", require("./routes/doctorRoute"));
+app.use("/api/nurses", require("./routes/nurseRoute"));
+app.use("/api/receptionists", require("./routes/receptionistRoute"));
+app.use("/api/appointments", require("./routes/appointmentRoute"));
+app.use("/api/chats", require("./routes/chatRoute"));
+app.use("/api/faqs", require("./routes/faqRoute"));
+app.use("/api/chat-sessions", require("./routes/sessionRoute"));
+app.use("/api/web", require("./routes/websiteRoute"));
+app.use("/api/jobs", require("./routes/jobRoute"));
+app.use("/api/stats", require("./routes/statsRoute"));
 
-  const userRoute = require("./routes/userRoute");
-  const roleRoute = require("./routes/roleRoute");
-  const permissionRoute = require("./routes/permissionRoute");
-  const patientRoute = require("./routes/patientRoute");
-  const doctorRoute = require("./routes/doctorRoute");
-  const nurseRoute = require("./routes/nurseRoute");
-  const receptionistRoute = require("./routes/receptionistRoute");
-  const appointmentRoute = require("./routes/appointmentRoute");
-  const chatRoute = require("./routes/chatRoute");
-  const faqRoute = require("./routes/faqRoute");
-  const sessionRoute = require("./routes/sessionRoute");
-
-  app.get("/", (req, res) => res.send("server is live"));
-
-
-  app.use("/api/user", userRoute);
-  app.use("/api/roles", roleRoute);
-  app.use("/api/permissions", permissionRoute);
-  app.use("/api/patients", patientRoute);
-  app.use("/api/doctors", doctorRoute);
-  app.use("/api/nurses", nurseRoute);
-  app.use("/api/receptionists", receptionistRoute);
-  app.use("/api/appointments", appointmentRoute);
-  app.use("/api/chats", chatRoute);
-  app.use("/api/faqs", faqRoute);
-  app.use("/api/chat-sessions", sessionRoute);
-  app.use("/api/web", require("./routes/websiteRoute"));
-  app.use("/api/jobs", require("./routes/jobRoute"));
-  app.use("/api/stats", require("./routes/statsRoute"));
-
-
-  app.listen(PORT, () => {
-      console.log(`Server started on ${PORT}`);
-      connectDb();
-  })
+module.exports = app; 
