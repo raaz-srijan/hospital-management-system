67bfe55fbb7a41504e85e060851cf61424a48826
raaz-srijan <srijannospace@gmail.com>
2025-12-25 19:28:10 +0545

    vercel

--- src/index.js
+++ src/index.js
@@ -7,30 +7,30 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const connectDb = require("./config/connectDb");
+const connectDb = require("../src/config/connectDb");
 
-connectDb();
-
-app.get("/test", (req, res) => {
+app.get("/", (req, res) => {
   res.send("server is live");
 });
 
-app.get("/test", (req, res) => {
-  res.send("server is live");
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/user", require("../src/routes/userRoute"));
+app.use("/api/roles", require("../src/routes/roleRoute"));
+app.use("/api/permissions", require("../src/routes/permissionRoute"));
+app.use("/api/patients", require("../src/routes/patientRoute"));
+app.use("/api/doctors", require("../src/routes/doctorRoute"));
+app.use("/api/nurses", require("../src/routes/nurseRoute"));
+app.use("/api/receptionists", require("../src/routes/receptionistRoute"));
+app.use("/api/appointments", require("../src/routes/appointmentRoute"));
+app.use("/api/chats", require("../src/routes/chatRoute"));
+app.use("/api/faqs", require("../src/routes/faqRoute"));
+app.use("/api/chat-sessions", require("../src/routes/sessionRoute"));
+app.use("/api/web", require("../src/routes/websiteRoute"));
+app.use("/api/jobs", require("../src/routes/jobRoute"));
+app.use("/api/stats", require("../src/routes/statsRoute"));
+
+app.listen(PORT, () => {
+  connectDb();
+  console.log(`Server started on ${PORT}`);
 });
-
-app.use("/api/user", require("./routes/userRoute"));
-app.use("/api/roles", require("./routes/roleRoute"));
-app.use("/api/permissions", require("./routes/permissionRoute"));
-app.use("/api/patients", require("./routes/patientRoute"));
-app.use("/api/doctors", require("./routes/doctorRoute"));
-app.use("/api/nurses", require("./routes/nurseRoute"));
-app.use("/api/receptionists", require("./routes/receptionistRoute"));
-app.use("/api/appointments", require("./routes/appointmentRoute"));
-app.use("/api/chats", require("./routes/chatRoute"));
-app.use("/api/faqs", require("./routes/faqRoute"));
-app.use("/api/chat-sessions", require("./routes/sessionRoute"));
-app.use("/api/web", require("./routes/websiteRoute"));
-app.use("/api/jobs", require("./routes/jobRoute"));
-app.use("/api/stats", require("./routes/statsRoute"));
-
